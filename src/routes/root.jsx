20373cd18c0065f192af7bb212fa84644872d745
ahadakash <sadhinakash181@gmail.com>
2023-10-04 23:58:40 +0600

    2

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,10 @@
-import { Link, Outlet, useLoaderData, } from "react-router-dom";
-import { getContacts } from "../contact";
+import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contact";
+
+export async function action() {
+    const contact = await createContact();
+    return { contact };
+}
 
 export async function loader() {
     const contacts = await getContacts();
@@ -31,9 +36,9 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {contacts.length ? (
